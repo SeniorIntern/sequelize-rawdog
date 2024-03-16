38e7292157176ec314805108292d550efc79b033
SeniorIntern <tnikhil.official@gmail.com>
2024-03-16 18:50:27 +0545

    Add CRUD api for airport resource

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,9 +5,11 @@ const { InfoController } = require('../../controllers');
 
 const airplaneRoutes = require('./airplane-routes');
 const cityRoutes = require('./city-routes');
+const airportRoutes = require('./airport-routes');
 
 router.use('/airplanes', airplaneRoutes);
 router.use('/cities', cityRoutes);
+router.use('/airports', airportRoutes);
 router.get('/info', InfoController.info);
 
 module.exports = router;
