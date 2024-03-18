60b241e4c92515b5fb5989601e9fa42704d5613b
SeniorIntern <tnikhil.official@gmail.com>
2024-03-18 19:28:51 +0545

    Apply prettier rules

--- src/repositories/queries.js
+++ src/repositories/queries.js
@@ -1,7 +1,7 @@
 function addRowLockOnFlights(flightId) {
-  return `SELECT * from Flights WHERE Flights.id = ${flightId} FOR UPDATE;`
+  return `SELECT * from Flights WHERE Flights.id = ${flightId} FOR UPDATE;`;
 }
 
 module.exports = {
   addRowLockOnFlights
-}
+};
