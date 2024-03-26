689e51ec7d4a7120c1827201cdb7252ad79c4783
SeniorIntern <tnikhil.official@gmail.com>
2024-03-26 13:21:16 +0545

    Make proxy compliant

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const { ServerConfig } = require('./config');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
+app.use('/flightsService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`listening on port:${ServerConfig.PORT}...`);
